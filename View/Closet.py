0e68c0a73386b85a6496bd843def16b7ab2d72e5
kny <kny030303>
2020-11-29 03:28:47 +0900

    add화면 수정 정보 나타내기

--- View/Closet.py
+++ View/Closet.py
@@ -15,12 +15,77 @@ from typing import TYPE_CHECKING
 closetUi = '../_uiFile/viewCloset.ui'
 
 class ClosetDialog(QDialog):
+
+
+
     def __init__(self):
         QDialog.__init__(self, None)
         uic.loadUi(closetUi, self)
-        # sys.exit(app.exec_())
+        self.img = ['','image:url(../image/cardigan.png);','image:url(../image/coat.png);','image:url(../image/fleece.png);','image:url(../image/hoodzip_up.png);'
+           ,'image:url(../image/jacket.png);','image:url(../image/padding.png);','image:url(../image/tem.png);']
+        self.clothes_labeles = []
+        self.name_labeles = []
+        self.season_labeles = []
+        self.temperature_labeles =[]
+        # for i in range(0,4):
+        #     label = QLabel("a",self)
+        #     label.setGeometry(20+i*320,170,171,211)
+        #     label.setStyleSheet('background-color: rgb(217, 197, 255)')
+        #     self.clothes_label.append(label)
+
+       # self.groupBox_1.hide()
         self.c_btn_main.clicked.connect(self.click_main)
         self.c_btn_add.clicked.connect(self.click_add)
+        self.file_read()
+
+    def file_read(self):
+        f = open('../File/userClothesInfo.txt','r', encoding='UTF-8')
+        try:
+            lines = f.readlines()
+            print(lines)
+            self.draw_ui(lines)
+        except FileNotFoundError as e:
+            print(e)
+        finally:
+            f.close()
+
+    def draw_ui(self,lines):
+        j = 0
+        for i in range(0,len(lines)):
+            line = lines[i].split('/')
+            name = line[0]
+            season = line[1]
+            temperature = line[2]
+            clothes = line[3]
+
+            if i>3:j=1
+            i = i%4
+
+            #옷
+            img_clothes = QLabel('',self)
+            img_clothes.setStyleSheet(self.img[int(clothes)])
+            img_clothes.setGeometry(20+i*320,170+300*j,171,211)
+            #img_clothes.setStyleSheet('background-color: rgb(217, 197, 255)')
+            self.clothes_labeles.append(img_clothes)
+
+            #이름
+            label_name = QLabel(name, self)
+            label_name.setAlignment(Qt.AlignCenter)
+            label_name.setGeometry(20 + i * 320, 370+300*j, 171, 51)
+            label_name.setStyleSheet('background-color: rgb(144, 216, 255)')
+            self.name_labeles.append(label_name)
+
+            #계절
+            label_season = QLabel(season, self)
+            label_season.setGeometry(200 + i * 320, 170+300*j, 121, 61)
+            label_season.setStyleSheet('background-color: rgb(144, 216, 255)')
+            self.name_labeles.append(label_season)
+
+            #온도
+            label_temperature = QLabel(temperature, self)
+            label_temperature.setGeometry(200 + i * 320, 240+300*j, 121, 61)
+            label_temperature.setStyleSheet('background-color: rgb(144, 216, 255)')
+            self.name_labeles.append(label_temperature)
 
 
     def click_main(self):
