4e15e5927c7d9677935e8a0a46d5c1d5e6ad840d
kny <kny030303>
2020-12-01 22:26:18 +0900

    finish

--- View/Pick_Area.py
+++ View/Pick_Area.py
@@ -15,7 +15,7 @@ from data.date import Date
 
 from View.Closet import ClosetDialog
 
-mainUi = '../_uiFile/viewMain.ui'
+mainUi = '../_uiFile/viewStart.ui'
 
 class MainDialog(QDialog):
     def __init__(self):
@@ -25,11 +25,8 @@ class MainDialog(QDialog):
         #날씨
         self.weather = Weather()
         self.weather.getWeather()
-        #날짜
-        self.date = Date()
+
         #클릭
-        self.m_btn_closet.clicked.connect(self.click_closet)
-        self.m_btn_reco.clicked.connect(self.click_recommend)
         self.m_btn_seoul.clicked.connect(lambda state,button = self.m_btn_seoul: self.click_area(state,button))
         self.m_btn_busan.clicked.connect(lambda state,button = self.m_btn_busan: self.click_area(state,button))
         self.m_btn_dejun.clicked.connect(lambda state,button = self.m_btn_dejun: self.click_area(state,button))
@@ -39,39 +36,14 @@ class MainDialog(QDialog):
         self.draw_ui()
 
 
-    def draw_ui(self):
-        # 년도
-        self.m_la_year.setText(str(self.date.getYear()))
-        self.m_la_day.setText(self.date.__str__())
-        # 온도
-        self.m_la_tem.setText(self.weather.getTemperature() + '°C')
-        # 상태
-        self.m_la_state.setText(self.weather.getComment())
-        #지역
-        self.m_la_area.setText(self.weather.getLocaion())
-
-    def get_weather(self):
-        return self.weather
 
     def click_area(self,state ,button):
         self.weather.setLocation(button.text())
         self.weather.getWeather()
         self.m_la_tem.setText(self.weather.getTemperature()+'°C')
         self.m_la_state.setText(self.weather.getComment())
-        self.m_la_area.setText(self.weather.getLocaion())
 
-    def click_closet(self):
-        self.accept()
-        r = ClosetDialog()
-        r.show()
-        r.exec_()
 
-    def click_recommend(self):
-        from View.Recommend import RecommendDialog
-        self.accept()
-        r = RecommendDialog(self.weather)
-        r.show()
-        r.exec_()
 
 
 
